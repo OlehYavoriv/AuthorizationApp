174a22dfacb6b0f294241d4b20d8ed55482b6f21
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-03-20 23:43:22 +0200

    Add main activity file

--- app/src/main/java/com/example/authorization/MainActivity.kt
+++ app/src/main/java/com/example/authorization/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.authorization
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.authorization.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+            .also { setContentView(it.root) }
     }
+
 }
